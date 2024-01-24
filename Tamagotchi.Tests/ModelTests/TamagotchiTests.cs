fda8baae21309ffaa46052db1195f10b795c16b8
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-01-24 09:23:08 -0800

    test for food and rest + passing test

--- Tamagotchi.Tests/ModelTests/TamagotchiTests.cs
+++ Tamagotchi.Tests/ModelTests/TamagotchiTests.cs
@@ -11,7 +11,7 @@ namespace TamagotchiProject.Tests
     [TestMethod]
     public void TamagotchiConstructor_CreatesInstanceOfTamagotchi_Tamagotchi()
   {
-    Tamagotchi newTamagotchi = new Tamagotchi("Test Tamagotchi",1,2);
+    Tamagotchi newTamagotchi = new Tamagotchi("Test Tamagotchi",1,2,3);
     Assert.AreEqual(typeof(Tamagotchi), newTamagotchi.GetType());
   }
   
@@ -19,7 +19,7 @@ namespace TamagotchiProject.Tests
     public void GetName_ReturnsName_String()
     {
       string name = "Test Tamagotchi";
-      Tamagotchi newTamagotchi = new Tamagotchi(name,2,3);
+      Tamagotchi newTamagotchi = new Tamagotchi(name,2,3,4);
       string result = newTamagotchi.Name;
       Assert.AreEqual(name, result);
     }
@@ -28,7 +28,7 @@ namespace TamagotchiProject.Tests
     public void GetFood_ReturnsFood_Int()
     {
       int food = 100;
-      Tamagotchi newTamagotchi = new Tamagotchi("Test Tamagotchi",food,3);
+      Tamagotchi newTamagotchi = new Tamagotchi("Test Tamagotchi",food,4,5);
       int result = newTamagotchi.Food;
       Assert.AreEqual(food, result);
     }
@@ -37,7 +37,36 @@ namespace TamagotchiProject.Tests
     public void GetAttention_ReturnsAttention_Int()
     {
       int attention = 100;
-      Tamagotchi newTamagotchi = new Tamagotchi("Test Tamagotchi",3,attention);
+      Tamagotchi newTamagotchi = new Tamagotchi("Test Tamagotchi",3,attention,5);
+      int result = newTamagotchi.Attention;
+      Assert.AreEqual(attention, result);
+    }
+
+    [TestMethod]
+    public void GetRest_ReturnsRest_Int()
+    {
+      int rest = 100;
+      Tamagotchi newTamagotchi = new Tamagotchi("Test Tamagotchi",3,4,rest);
+      int result = newTamagotchi.Rest;
+      Assert.AreEqual(rest, result);
+    }
+
+    [TestMethod]
+    public void SetFood_SetsFood_Int()
+    {
+      int food = 85;
+      Tamagotchi newTamagotchi = new Tamagotchi("Test Tamagotchi",0,4,5);
+      newTamagotchi.Food = food;
+      int result = newTamagotchi.Food;
+      Assert.AreEqual(food, result);
+    }
+
+    [TestMethod]
+    public void SetAttention_SetsAttention_Int()
+    {
+      int attention = 85;
+      Tamagotchi newTamagotchi = new Tamagotchi("Test Tamagotchi",0,4,5);
+      newTamagotchi.Attention = attention;
       int result = newTamagotchi.Attention;
       Assert.AreEqual(attention, result);
     }
--- Tamagotchi/Models/Tamagotchi.cs
+++ Tamagotchi/Models/Tamagotchi.cs
@@ -2,14 +2,18 @@ namespace TamagotchiProject.Models
 {
   public class Tamagotchi
   {
-    public Tamagotchi(string tamagotchiName, int foodLevel, int attentionLevel)
+    public Tamagotchi(string tamagotchiName, int foodLevel, int attentionLevel, int restLevel)
     {
       Name = tamagotchiName;
       Food = foodLevel;
       Attention = attentionLevel;
+      Rest = restLevel;
+
     }
     public string Name { get; }
-    public int Food { get; }
-    public int Attention { get; }
+    public int Food { get; set; }
+    public int Attention { get; set; }
+    public int Rest { get; }
+
   }
 }
